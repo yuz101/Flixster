74b49ca6ae6d9349238cbf8da335d3c70cd670c6
Yuru Zhou <yuz101@ucsd.edu>
2020-09-25 22:31:20 -0700

    Initial Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Yuru Zhou on 9/25/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
